3f3f886fbb5a7f4642933e371a35758ba71a1aba
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-12-25 09:57:06 +0530

    Create script.js

---
+++ JavaScript Intro/script.js
@@ -0,0 +1,21 @@
+alert('Welcome in my first webpage of JS.')
+      
+      console.log("Now! Code is running...")
+      console.log("What's code, Awesome")
+      
+      console.log("Looking like a wow")
+      
+      var Istrue = confirm("Please sure that Mibile No. must be correct!")
+      var a = prompt("Enter your mobile Number")
+      if(a.length === 10 && Istrue){
+        alert("Right Mobile No.") 
+        console.log("You are enter in this webpage")
+        console.log("Your Mobile No. is " + a)
+      }
+      
+      else{
+        alert("Wrong Mobile No.\nTry Again.")
+        alert("Next time write the correct Mobile No.")
+        console.log("Wrong Mobile No.")
+      }
+     // document.body.style.backgroundColor = "yellowgreen"
