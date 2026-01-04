0de08737f55696a29e24d48fbc9daac7ed469808
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-04 06:28:57 +0530

    Create var.js

---
+++ JS - Variables, Data Types & Objects/var.js
@@ -0,0 +1,7 @@
+console.log("Hello! This is the tutorial 55.")
+    
+    var a = 13;
+    var b = 12;
+    var c = "Happy";
+    console.log(a + b + 12)
+    console.log(typeof a, typeof b, typeof c)
