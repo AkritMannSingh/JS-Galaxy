e8f317606400bd7004f04fb0dfbe0bc5185e1322
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-10 22:11:56 +0530

    Create data-types.js

---
+++ JS - Variables, Data Types & Objects/data-types.js
@@ -0,0 +1,12 @@
+let g = "Enthusiast";
+let h = 11;
+let i = 2.11;
+const k = true;
+
+let x = undefined;
+let y = null;
+
+console.log(g, h, i, k, x, y)
+console.log(typeof g,typeof h,typeof i,typeof k,typeof x,typeof y)
+
+// typeof null is always Object.
