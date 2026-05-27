eb913cb827b5685d0b2c8da691505d39eb2b592c
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-05-27 22:28:35 +0530

    Update 2.6_Interaction.js

--- JS Fundamentals/2.6_Interaction.js
+++ JS Fundamentals/2.6_Interaction.js
@@ -1,1 +1,13 @@
+// In this we will learn about - "Interaction: alert, prompt, confirm"
+
+// Alert :-
+
+alert('Welcome to Interaction Section!');
+
+// Prompt :-
+
+let name = prompt('What your name?');
+let a = prompt(`Welcome ${name} How are you!`);
+
+alert('Nice to meet you');
 
